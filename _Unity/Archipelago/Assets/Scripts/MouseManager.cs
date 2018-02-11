86095f8eecebf753b7235eaa48f150293a0d98c3
Peter Sousa <peter.sousa@dal.ca>
2018-02-11 13:23:27 -0400

    Territory Acquisition

--- _Unity/Archipelago/Assets/Scripts/MouseManager.cs
+++ _Unity/Archipelago/Assets/Scripts/MouseManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour {
 
+	public Sprite incomeCount;
+	public Sprite tileGrassOwned;	// 2	
+	public Sprite tileSandOwned;	// 1
+	public Sprite tileTreeOwned;	// 3
+	public Sprite tileRockOwned;	// 4
+
 	// Update is called once per frame
 	void Update () {
 
@@ -27,12 +33,27 @@ public class MouseManager : MonoBehaviour {
             //If left mouse button pressed, only calls once on initial press(e.g not constantly calling on hold)
             if (Input.GetMouseButtonDown(0))
             {
+				Debug.Log ("Hello World");
                 //List of direct cell neighbors
+				/*
                 List<GameObject> neighbors = collidedHitInfo.GetComponent<Hex>().getNeighbors();
                 for (int i = 0; i < neighbors.Count; i++)
                 {
                     Debug.Log("X: " + neighbors[i].GetComponent<Hex>().x + " Y: " +neighbors[i].GetComponent<Hex>().y);
                 }
+                */
+				if (collidedHitInfo.tag == "Grass") {
+					collidedHitInfo.GetComponent<SpriteRenderer> ().sprite = tileGrassOwned;
+				}
+				else if (collidedHitInfo.tag == "Sand") {
+					collidedHitInfo.GetComponent<SpriteRenderer> ().sprite = tileSandOwned;
+				}
+				else if (collidedHitInfo.tag == "Tree") {
+					collidedHitInfo.GetComponent<SpriteRenderer> ().sprite = tileTreeOwned;
+				}
+				else if (collidedHitInfo.tag == "Rock") {
+					collidedHitInfo.GetComponent<SpriteRenderer> ().sprite = tileRockOwned;
+				}
             }
         }
 	}
