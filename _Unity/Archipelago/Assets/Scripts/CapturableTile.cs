cb98f1e1fbdaa34b8c03684e8fe3c76741a7e100
Connor <connorw.mcleod@gmail.com>
2018-02-22 17:17:12 -0400

    #48 Updating test scene

--- _Unity/Archipelago/Assets/Scripts/CapturableTile.cs
+++ _Unity/Archipelago/Assets/Scripts/CapturableTile.cs
@@ -4,34 +4,41 @@ using UnityEngine.UI;
 
 public class CapturableTile: MonoBehaviour{
 
+	// Sprites representing progress bar of each player
+	private Image captureBoarder; //this image displays the sprites
 	public Sprite p1CaptureBorder;
 	public Sprite p2CaptureBorder;
-	public bool paused = false;
+
+	// This is the hex script on the same tile which holds information about who owns the tile?
+
+	// Factors determining how long it takes to capture a tile
 	public float captureSpeedPerUnit = 1;
-	public float neutralUnitFactor = 0.25f;
-
-	private Image captureBoarder;
+	public float neutralUnitFactor = 0.25f; //this is how fast the tile will reset if nothing is on it
 	const float totalCaptureCost = 10;
+
+	//Current state of capturing the tile
 	private float amountCaptured = 0;
+	public bool paused = false;
 
-
-	//store the number of units on a hex to determine if it should start a capturing proccess
+	//number of units on a hex. used to progress state of capturing proccess
 	public int numP1UnitsOnHex = 0;
 	public int numP2UnitsOnHex = 0;
 
+	//Initializeation
 	public void Start(){
 		captureBoarder = GetComponent<Image>();
+		// This is mostly for testing. Fill Amount should be set to 0 so the tiles starts neutral.
 		amountCaptured = captureBoarder.fillAmount * totalCaptureCost;
 	}
 	
 	// Update is called once per frame
 	void Update (){
-
+		// progress tile capturing each frame
 		progressTileCapture ();
 	}
 
 
-
+	// Unit calls this when it enters the tile
 	public void addUnits(int numUnits, bool player1){
 		if(tileOwned){return;}
 
@@ -49,6 +56,7 @@ public class CapturableTile: MonoBehaviour{
 		}
 	}
 
+	// Unit calls this when it leaves the tile
 	public void removeUnit(int numUnits, bool player1){
 		if(tileOwned){return;}
 
@@ -61,6 +69,7 @@ public class CapturableTile: MonoBehaviour{
 
 
 	bool tileOwned = false;
+	// Adnvace state towards capturing the tile
 	private void progressTileCapture(){
 
 		if (!tileOwned && captureBoarder.enabled) {
