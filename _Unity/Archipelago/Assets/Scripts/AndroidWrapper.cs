751f6b156cf51610b17c907c72ec3243fce0d50b
Connor <connorw.mcleod@gmail.com>
2018-03-14 21:34:40 -0300

    #24 Adding unity as library in android studio

---
+++ _Unity/Archipelago/Assets/Scripts/AndroidWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidWrapper : MonoBehaviour {
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (10, 10, 150, 100), "Hello World")) {
+			print ("You clicked the unity button!");
+			AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject currentActivity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+
+			currentActivity.Call("trigger");
+		}
+
+	}
+}
