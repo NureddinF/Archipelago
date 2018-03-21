3d2efeeb9f0516b23c96b5c0844786f06de70ee0
Connor <connorw.mcleod@gmail.com>
2018-03-21 17:06:58 -0300

    WIP: #73 #74 commiting work before going home. Adding some scenes for multiplay things

--- _Unity/Archipelago/Assets/Scripts/AndroidWrapper.cs
+++ _Unity/Archipelago/Assets/Scripts/AndroidWrapper.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class AndroidWrapper : MonoBehaviour {
 
+	public static int intitalScene = 0;
+
+	private static Dictionary<string, int> scenes = new Dictionary<string, int>{
+		{ "play", 1 },
+		{ "host", 2 },
+		{ "join", 3 }
+	};
+
 	public static void winAction(){
+		AndroidJavaObject currentActivity = getUnityActivity();
+
+		currentActivity.Call("win");
+	}
+
+	public static int getScene(){
+
+		AndroidJavaObject currentActivity = getUnityActivity();
+
+		string scene = currentActivity.Call<string>("getScene");
+		intitalScene = scenes [scene];
+		return intitalScene;
+	}
+
+
+	private static AndroidJavaObject getUnityActivity(){
 		AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
 		AndroidJavaObject currentActivity = jc.GetStatic<AndroidJavaObject>("currentActivity");
 
-		currentActivity.Call("win");
+		return currentActivity;
 	}
 }
---
+++ _Unity/Archipelago/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Won't be able to call android functions when testing in unity os turn on debug and set scene manually
+	public bool isDebug = false;
+	public int debugScene = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+		int sceneToLoad = debugScene;
+		if (!isDebug) {
+			sceneToLoad = AndroidWrapper.getScene ();
+		}
+		SceneManager.LoadScene (sceneToLoad);
+	}
+}
